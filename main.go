aecaa5827d134a6a0ddeaf9e2d8601c2b44c8472
Robert Tucker <r.tucker@quadient.com>
2022-09-18 21:34:41 -0400

    [chore] add Cobra & Viper

---
+++ main.go
@@ -0,0 +1,14 @@
+//
+// Copyright (c) 2022 Quadient Group AG
+//
+// This file is subject to the terms and conditions defined in the
+// 'LICENSE' file found in the root of this source code package.
+//
+
+package main
+
+import "github.com/robertwtucker/spt-util/cmd"
+
+func main() {
+	cmd.Execute()
+}
