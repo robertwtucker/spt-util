7b74848dfd16979d99ebb33b6a652c39bcc0f2f2
Robert Tucker <r.tucker@quadient.com>
2023-03-29 10:55:38 -0400

    feat: add simple, in-memory event bus

---
+++ internal/eventbus/events.go
@@ -0,0 +1,14 @@
+//
+// Copyright (c) 2023 Quadient Group AG
+//
+// This file is subject to the terms and conditions defined in the
+// 'LICENSE' file found in the root of this source code package.
+//
+
+package eventbus
+
+// Events for demo init command.
+const InitDeployScalerWorkflows = "init-deploy-scaler-workflows"
+const InitFindScalerWorkflows = "init-find-scaler-workflows"
+const InitStart = "init-start"
+const InitUploadChangeSet = "init-upload-changeset"
