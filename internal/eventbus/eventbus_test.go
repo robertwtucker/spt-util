d633e18c5ef7b9d5042388cc21370fb1889c941e
Robert Tucker <r.tucker@quadient.com>
2023-03-29 12:32:00 -0400

    test: add tests for eventbus

---
+++ internal/eventbus/eventbus_test.go
@@ -0,0 +1,79 @@
+//
+// Copyright (c) 2023 Quadient Group AG
+//
+// This file is subject to the terms and conditions defined in the
+// 'LICENSE' file found in the root of this source code package.
+//
+
+package eventbus_test
+
+import (
+	"testing"
+
+	"github.com/robertwtucker/spt-util/internal/eventbus"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEventBus_NewEventBus(t *testing.T) {
+	if eb := eventbus.NewEventBus(); eb == nil {
+		t.Fail()
+	}
+}
+
+func TestEventBus_NewEventChannel(t *testing.T) {
+	if ec := eventbus.NewEventChannel(); ec == nil {
+		t.Fail()
+	}
+}
+
+func TestEventBus_SubscribeEvent(t *testing.T) {
+	eventName := "foo"
+
+	eb := eventbus.NewEventBus()
+	_ = eb.SubscribeEvent(eventName)
+
+	assert.Equal(t, true, eb.HasSubscribers(eventName))
+}
+
+func TestEventBus_SubscribeEventChannel(t *testing.T) {
+	eventName := "foo"
+
+	eb := eventbus.NewEventBus()
+	ec := eventbus.NewEventChannel()
+	eb.SubscribeEventChannel(ec, eventName)
+
+	assert.Equal(t, true, eb.HasSubscribers(eventName))
+}
+
+func TestEventBus_PublishEvent(t *testing.T) {
+	eventName := "foo"
+	eventData := "bar"
+
+	eb := eventbus.NewEventBus()
+	ec := eb.SubscribeEvent(eventName)
+
+	go func() {
+		event := <-ec
+		defer event.Done()
+
+		assert.Equal(t, eventName, event.Name)
+		assert.Equal(t, eventData, event.Data)
+	}()
+
+	eb.PublishEvent(eventName, eventData)
+}
+
+func TestEventBus_SubscribeEventCallback(t *testing.T) {
+	eventName := "foo"
+	eventData := "bar"
+
+	eb := eventbus.NewEventBus()
+	eb.SubscribeEventCallback(
+		eventName,
+		func(name string, data interface{}) {
+			assert.Equal(t, eventName, name)
+			assert.Equal(t, eventData, data)
+		})
+
+	eb.PublishEvent(eventName, eventData)
+}
